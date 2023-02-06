7cdf2eaae641a4fe504cdd4abb551ba043f63566
Artormvladis <112126702+Artormvladis@users.noreply.github.com>
2023-02-06 14:06:58 +0300

    Update vector_list_1.cpp

--- vector_list_1.cpp
+++ vector_list_1.cpp
@@ -1,1 +1,18 @@
-
+#include "eaasy_list.h"
+void itc_rev_list(vector <int> &mass){
+    if(mass.size>0){
+      int a,b,c;
+      b=mass.size;
+        for(int i=mass; i<=b//2; i++){
+          a=mass[i];
+          c=mass[b-i];
+          mass[i]=c;
+          mass[b-i]=a;
+        }
+    }
+}
+void itc_rev_par_list(vector <int> &mass){
+  if(mass.size>0){
+    
+  }
+}
