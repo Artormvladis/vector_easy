db92eaaea4755656a7eddc025cdf542f3f83f83a
Artormvladis <112126702+Artormvladis@users.noreply.github.com>
2023-02-06 14:20:33 +0300

    Update vector_list.cpp

--- vector_list.cpp
+++ vector_list.cpp
@@ -1,31 +1,36 @@
 #include "easy_list.h"
 void itc_even_index_list (const vector <int> &mass, vector <int> &mass2){
-  for(int i=0; i<mass.size(); i+=2)
+  for(int i=0; i<mass.size(); i+=2){
     mass2.push_back(mass[i]);
-}
+  }}
 void itc_even_parts_list(const vector <int> &mass, vector <int> &mass2){
   for(int i=0; i<mass.size(); ++i){
     if (mass[i]%2==0){
       mass2.push_back(mass[i]);
-    }
-  }
-}
+    }}}
 int itc_positive_list(const vector <int> &mass){
   int a=0;
-  for (int i=0; i<mass.size(); i++)
-    if (mass[i]>0) a++;
+  for (int i=0; i<mass.size(); i++){
+    if (mass[i]>0){
+      a++;
+    }}
   return a;
 }
 int itc_sl_list(const vector <int> &mass){
   int a=0;
-  for(int i=1; i<mass.size(); ++i)
-    if(mass[i]>mass[i-1]) a++;
+  for(int i=1; i<mass.size(); ++i){
+    if(mass[i]>mass[i-1]){
+      a++;
+    }}
   return a;
 }
 bool itc_same_parts_list(const vector <int> &mass){
   for(int i=1; i<mass.size(); i++){
-    if(mass[i]>=0 && mass[i-1]>=0) return true;
-    else if(mass[i]<0 && mass[i-1] < 0) return true;
-  }
+    if(mass[i]>=0 && mass[i-1]>=0){
+      return true;
+    }
+    else if(mass[i]<0 && mass[i-1] < 0){
+      return true;
+    }}
   return false;
 }
