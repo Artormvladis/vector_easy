4091239843fcebc5e7c6416b0b3735bb71a3d850
Artormvladis <112126702+Artormvladis@users.noreply.github.com>
2023-02-06 15:01:49 +0300

    Update vector_list_1.cpp

--- vector_list_1.cpp
+++ vector_list_1.cpp
@@ -1,19 +1,26 @@
 #include "eaasy_list.h"
 void itc_rev_list(vector <int> &mass){
-    if(mass.size>0){
-      int a,b,c;
-      b=mass.size;
+    if(mass.size()>0){
+      int a,c;
         for(int i=mass; i<=b//2; i++){
           a=mass[i];
-          c=mass[b-i];
-          mass[i]=c;
-          mass[b-i]=a;
-        }
-    }
-}
+          mass[i]=mass[mass.size()-1];
+          mass[mass.size()-1]=a;
+        }}}
 void itc_rev_par_list(vector <int> &mass){
-  if(mass.size>0){
+  if(mass.size()>0){
     int a;
-      for(int i=0;)
-  }
-}
+      for(int i=0; i<mass.size()-1, i=i+2){
+          a=mass[i];
+          mass[i]=mass[i+1];
+          mass[i+1]=a;
+      }}}
+void itc_rshift_list(vector <int> &mass){
+    if(mass.size()>0){
+        int a,b;
+        for(int i=1; i<=mass.size(); i++){
+            a=mass[i-1];
+            b=mass[i];
+            mass[i]=mass[i-1];
+            
+        }}}
