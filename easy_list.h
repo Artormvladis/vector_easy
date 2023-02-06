35d5e19ffd28a71ab70f5fbd7c9b459bcb454070
Artormvladis <112126702+Artormvladis@users.noreply.github.com>
2023-02-06 13:42:29 +0300

    Update easy_list.h

--- easy_list.h
+++ easy_list.h
@@ -9,4 +9,5 @@ void itc_even_parts_list(const vector <int> &mass, vector <int> &mass2);
 int itc_positive_list(const vector <int> &mass);
 int itc_sl_list(const vector <int> &mass);
 bool itc_same_parts_list(const vector <int> &mass);
+void itc_rev_list(vector <int> &mass);
 #endif // VECTOR_LIST_H_INCLUDED
