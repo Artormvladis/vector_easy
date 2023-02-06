4d037f9e8ac200ab15b5654d9f750bdad5ae660e
Artormvladis <112126702+Artormvladis@users.noreply.github.com>
2023-02-06 13:33:28 +0300

    Update vector_list.cpp

--- vector_list.cpp
+++ vector_list.cpp
@@ -1,4 +1,4 @@
-#include "vector_list.h"
+#include "easy_list.h"
 void itc_even_index_list (const vector <int> &mass, vector <int> &mass2){
   for(int i=0; i<mass.size(); i+=2)
     mass2.push_back(mass[i]);
