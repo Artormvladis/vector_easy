8c36318ec6cafaa69c9f60cdd8a16b73f07add7b
Artormvladis <112126702+Artormvladis@users.noreply.github.com>
2023-02-13 22:04:07 +0300

    Update vector_list_1.cpp

--- vector_list_1.cpp
+++ vector_list_1.cpp
@@ -1,7 +1,7 @@
 #include "easy_list.h"
 void itc_rev_list(vector <int> &mass){
     if(mass.size()>0){
-      int a,c;
+      int a;
         for(int i=0; i<=mass.size()/2; i++){
           a=mass[i];
           mass[i]=mass[mass.size()-1];
