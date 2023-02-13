20b39491efd7ce9ec8b1e262d705f217b6d13b0e
Artormvladis <112126702+Artormvladis@users.noreply.github.com>
2023-02-13 22:39:56 +0300

    Update vector_list_1.cpp

--- vector_list_1.cpp
+++ vector_list_1.cpp
@@ -18,7 +18,7 @@ void itc_rev_par_list(vector <int> &mass){
 void itc_rshift_list(vector <int> &mass){
     if(mass.size()>0){
         int a;
-        a=mass[mass.size()];
+        a=mass[mass.size()-1];
         for(int i=mass.size()-1; i>0; i--){
             mass[i]=mass[i-1];
         }
@@ -37,5 +37,5 @@ void itc_super_shift_list(vector <int> &mass, int n){
                 a=mass[0];
                 for(int i1=1; i<mass.size(); i1++){
                     mass[i1-1]=mass[i1];
-                    mass[mass.size()]=a;
+                    mass[mass.size()-1]=a;
                 }}}}}
